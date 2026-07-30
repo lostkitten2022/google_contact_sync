ea07829b093d6921ac94ff761df4ed5cde4bee1c
lostkitten2022 <108204683+lostkitten2022@users.noreply.github.com>
2026-07-30 09:55:11 +0800

    Update SyncController.php

--- lib/Controller/SyncController.php
+++ lib/Controller/SyncController.php
@@ -1,1 +1,37 @@
+<?php
+namespace OCA\GoogleContactSync\Controller;
 
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\IConfig;
+use OCP\IRequest;
+
+class SyncController extends Controller {
+    private $config;
+    private $userId;
+
+    public function __construct($AppName, IRequest $request, IConfig $config, $userId) {
+        parent::__construct($AppName, $request);
+        $this->config = $config;
+        $this->userId = $userId;
+    }
+
+    /**
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     */
+    public function getStatus() {
+        $enabled = $this->config->getUserValue($this->userId, 'google_contact_sync', 'enabled', 'no');
+        return new DataResponse(['enabled' => $enabled === 'yes']);
+    }
+
+    /**
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     */
+    public function setStatus($enabled) {
+        $val = ($enabled === 'true' || $enabled === true) ? 'yes' : 'no';
+        $this->config->setUserValue($this->userId, 'google_contact_sync', 'enabled', $val);
+        return new DataResponse(['status' => 'success', 'enabled' => $val]);
+    }
+}
