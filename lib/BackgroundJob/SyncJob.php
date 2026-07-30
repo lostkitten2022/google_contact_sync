ca63b61a0e16db90dcfdeb343985a500e43498a4
lostkitten2022 <108204683+lostkitten2022@users.noreply.github.com>
2026-07-30 09:54:37 +0800

    Update SyncJob.php

--- lib/BackgroundJob/SyncJob.php
+++ lib/BackgroundJob/SyncJob.php
@@ -1,1 +1,20 @@
+<?php
+namespace OCA\GoogleContactSync\BackgroundJob;
 
+use OCP\BackgroundJob\TimedJob;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCA\GoogleContactSync\Service\ContactSyncService;
+
+class SyncJob extends TimedJob {
+    private $syncService;
+
+    public function __construct(ITimeFactory $time, ContactSyncService $syncService) {
+        parent::__construct($time);
+        $this->syncService = $syncService;
+        $this->setInterval(3600); // 1 小时检测一次
+    }
+
+    protected function run($argument): void {
+        $this->syncService->syncAllUsers();
+    }
+}
