dc79c940eaf6ddc94b848078a0f9f0cb34cffaff
lostkitten2022 <108204683+lostkitten2022@users.noreply.github.com>
2026-07-30 09:55:39 +0800

    Update routes.php

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,1 +1,7 @@
-
+<?php
+return [
+    'routes' => [
+        ['name' => 'sync#getStatus', 'url' => '/status', 'verb' => 'GET'],
+        ['name' => 'sync#setStatus', 'url' => '/status', 'verb' => 'POST']
+    ]
+];
